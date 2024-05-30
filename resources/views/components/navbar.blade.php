e188d45e92bbfaedd7b85570117bff8ad00fda8c
apol yuzuguldu <vargakeri@icloud.com>
2024-05-30 22:11:44 +0200

    modifica nav

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,15 +1,13 @@
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
+<nav style="position: absolute;" class="navbar navbar-expand-lg bg-body-tertiary">
     <div class="container-fluid">
         <a class="navbar-brand" href="{{ route('home') }}">ThePost</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
             aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
+        <div  id="navbarNav">
             <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="{{ route('home') }}">Home</a>
-                </li>
+            
                 <li class="nav-item">
                     <a class="nav-link" href="#"></a>
                 </li>
@@ -53,6 +51,7 @@
                         </ul>
                     </li>
                 @endauth
+
                 @guest
                     <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
@@ -65,6 +64,7 @@
                         </ul>
                     </li>
                 @endguest
+
             </ul>
         </div>
         <form class="d-flex" method="GET" action="{{ route('article.search') }}">

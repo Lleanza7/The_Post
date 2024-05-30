65798888a315d1e9865987c19be542e007b02a2a
apol yuzuguldu <vargakeri@icloud.com>
2024-05-30 23:46:52 +0200

    creazione nuova nav + header (routa test)

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -1,3 +1,5 @@
-<x-navbar-new>
-
-</x-navbar-new>
+<header>
+    <x-navbar-new />
+    <p>LUCA & APOL</p>
+    <H2>Notizie globali e voci locali, il tuo <br> spazio per informarti e esprimerti.</H2>
+</header>

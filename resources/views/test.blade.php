15f3affa68cf05cb9e4d22d5d23d1809d8be107c
apol yuzuguldu <vargakeri@icloud.com>
2024-06-01 20:44:14 +0200

    aggiunta sezione categorie

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -1,12 +1,109 @@
-<header>
-    <x-navbar-new />
+<!DOCTYPE html>
+<html lang="en">
 
- 
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+
+<body>
+    <header>
+        <x-navbar-new />
         <p>LUCA & APOL</p>
         <H2>Notizie globali e voci locali, il tuo <br> spazio per informarti e esprimerti.</H2>
+        <div class="containerFeautured">
+            <h6>IN EVIDENZA</h6>
+            <p>Politica - Economia - Sport - Tecnologia - Arte </p>
+        </div>
+    </header>
+    <main>
+        <div class="test">
+            <div class="boxDivImg">
+                <div class="divImg"
+                    style=" background-image:url(https://external-preview.redd.it/O8Cq7qG0goqK4qz2DhodBsMEdRJk8r24CSPCnW43SiM.jpg?width=640&crop=smart&auto=webp&s=41d40f136132b1119a981720273ab060d16f37ca);">
+                </div>
+            </div>
+            <div class="boxCards">
+                <div class="containerCard1">
+                    <div class="cardHomeLarge"></div>
+                    <div class="cardHomeSmall"></div>
+                    <div class="cardHomeLarge"></div>
+                    <div class="cardHomeSmall"></div>
+                </div>
+                <div class="containerCard2">
+                    <div class="cardHomeSmall"></div>
+                    <div class="cardHomeLarge"></div>
+                    <div class="cardHomeSmall"></div>
+                    <div class="cardHomeLarge"></div>
+                </div>
+            </div>
+        </div>
+        <div class="test">
+            <div class="boxCards">
+                <div class="containerCard2">
+                    <div class="cardHomeLarge"></div>
+                    <div class="cardHomeSmall"></div>
+                    <div class="cardHomeLarge"></div>
+                    <div class="cardHomeSmall"></div>
+                </div>
+                <div class="containerCard1">
+                    <div class="cardHomeSmall"></div>
+                    <div class="cardHomeLarge"></div>
+                    <div class="cardHomeSmall"></div>
+                    <div class="cardHomeLarge"></div>
+                </div>
+            </div>
+            <div class="boxDivImg">
+                <div class="divImg"
+                    style="background-image:url(https://wallpaperswide.com/download/apple_on_black_background-wallpaper-1400x1050.jpg);">
+                </div>
+            </div>
+        </div>
+        <div class="test">
+            <div class="boxDivImg">
+                <div class="divImg"
+                    style="background-image:url(https://i.pinimg.com/736x/7e/aa/a0/7eaaa0c512de8516c39ce626167cb027.jpg);">
+                </div>
+            </div>
+            <div class="boxCards">
+                <div class="containerCard1">
+                    <div class="cardHomeLarge"></div>
+                    <div class="cardHomeSmall"></div>
+                    <div class="cardHomeLarge"></div>
+                    <div class="cardHomeSmall"></div>
+                </div>
+                <div class="containerCard2">
+                    <div class="cardHomeSmall"></div>
+                    <div class="cardHomeLarge"></div>
+                    <div class="cardHomeSmall"></div>
+                    <div class="cardHomeLarge"></div>
+                </div>
+            </div>
+        </div>
+        <div style="position:-webkit-sticky;position:sticky;top:0;"class="test">
+            <div class="boxCards">
+                <div class="containerCard2">
+                    <div class="cardHomeLarge"></div>
+                    <div class="cardHomeSmall"></div>
+                    <div class="cardHomeLarge"></div>
+                    <div class="cardHomeSmall"></div>
+                </div>
+                <div class="containerCard1">
+                    <div class="cardHomeSmall"></div>
+                    <div class="cardHomeLarge"></div>
+                    <div class="cardHomeSmall"></div>
+                    <div class="cardHomeLarge"></div>
+                </div>
+            </div>
+            <div class="boxDivImg">
+                <div class="divImg"
+                    style="background-image:url(https://i.pinimg.com/736x/ab/5c/a5/ab5ca5441028a548a87120025f0e78d1.jpg);">
+                </div>
+            </div>
+        </div>
+    </main>
+</body>
 
-   <div class="containerFeautured">
-        <h6>IN EVIDENZA</h6>
-        <p>Politica - Economia - Sport - Tecnologia - Arte </p>
-   </div>
-</header>
+</html>

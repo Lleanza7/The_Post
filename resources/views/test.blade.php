25924263b7e4f6452b7f3c82a49f2d190beec4a5
apol yuzuguldu <vargakeri@icloud.com>
2024-06-02 18:25:33 +0200

    fixx vari problemi

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -14,8 +14,8 @@
         <p>LUCA & APOL</p>
         <H2>Notizie globali e voci locali, il tuo <br> spazio per informarti e esprimerti.</H2>
         <div class="containerFeautured">
-            <h6>IN EVIDENZA</h6>
-            <p>Politica - Finanza - Sport - Tech</p>
+                <h6>IN EVIDENZA</h6>
+                <p>Politica - Finanza - Sport - Tech</p>
         </div>
     </header>
     <main>

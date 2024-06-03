53b70735924a98f248c67d8c3845bee0d37f0159
apol yuzuguldu <vargakeri@icloud.com>
2024-06-03 02:46:45 +0200

    completamento della homepage

--- resources/views/components/navbar-new.blade.php
+++ resources/views/components/navbar-new.blade.php
@@ -27,14 +27,16 @@
                     <h1> THE|POST</h1>
                 </div>
                 <form class="search" method="GET" action="{{route('article.search')}}">
-                    @csrf
-                    <svg xmlns="http://www.w3.org/2000/svg" width="17" height="17" fill="currentColor"
-                        class="bi bi-search" viewBox="0 0 16 16">
-                        <path
-                            d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0" />
-                    </svg>
-
-                    <input type="search" aria-label="Search" placeholder="Cerca un articolo" name="query">
+              <div style="display: flex;align-items: center;">
+                        @csrf
+                        <svg xmlns="http://www.w3.org/2000/svg" width="17" height="17" fill="currentColor"
+                            class="bi bi-search" viewBox="0 0 16 16">
+                            <path
+                                d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0" />
+                        </svg>
+    
+                        <input type="search" aria-label="Search" placeholder="Cerca un articolo" name="query">
+              </div>
                 </div>
             </form>
 
@@ -104,6 +106,7 @@
         </div>
 
     </nav>
+    
 
 </body>
 
--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -6,13 +6,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+
 </head>
 
 <body>
     <header>
         <x-navbar-new />
         <p>LUCA & APOL</p>
-        <H2>Notizie globali e voci locali, il tuo <br> spazio per informarti e esprimerti.</H2>
+        <h2>Notizie globali e voci locali, il tuo <br>
+            spazio per <span
+                 class="txt-rotate"
+                 data-period="1000"
+                 data-rotate='[ "informarti","esprimerti.", "manifestarti.", "raccontarti." ]'> </span>
+            </h2>
         <div class="containerFeautured">
                 <h6>IN EVIDENZA</h6>
                 <p>Politica - Finanza - Sport - Tech</p>
@@ -21,10 +27,15 @@
     <main>
 
 
-        <div class="test">
-            <div class="boxDivImg">
-                <div  class="divImg"
-                    style="  background-image: url(./storage/POLITICA.png);">
+        <div style="background-color: #E8ECEF;" class="test">
+            <div  class="boxDivImg">
+                <div  class="divImg">
+                   <div class="boxDivImgGlass" >
+                        <h2>  POLITICA</h2>
+                        <p>Tieniti aggiornato sulle ultime novità politiche a livello locale, nazionale e internazionale. Scopri analisi, opinioni e approfondimenti su governi, elezioni, politiche pubbliche e molto altro.
+                        </p>
+                   </div>
+
                 </div>
             </div>
             <div class="boxCards">
@@ -199,16 +210,22 @@
                 </div>
             </div>
             <div class="boxDivImg">
-                <div class="divImg"
-                style="  background-image: url(./storage/FINANZA.png);">
+                <div class="divImg" >
+               <div class="boxDivImgGlass">
+                    <h2>  FINANZA</h2>
+                    <p>Rimani al passo con le ultime notizie finanziarie, dai mercati globali agli investimenti personali. Leggi analisi di esperti, tendenze economiche e consigli per gestire al meglio le tue finanze.</p>
+               </div>
                 </div>
             </div>
         </div>
 
-        <div class="test">
+        <div style="background-color: #E8ECEF" class="test">
             <div class="boxDivImg">
-                <div class="divImg"
-                style="  background-image: url(./storage/SPORT.png);">
+                <div class="divImg" >
+               <div class="boxDivImgGlass">
+                    <h2> SPORT</h2>
+                    <p>Esplora il mondo dello sport con notizie, risultati e analisi delle tue discipline preferite. Dalle competizioni internazionali ai campionati locali, resta informato su tutto ciò che accade nel mondo dello sport.</p>
+               </div>
                 </div>
             </div>
             <div class="boxCards">
@@ -383,8 +400,11 @@
                 </div>
             </div>
             <div class="boxDivImg">
-                <div class="divImg"
-                style="  background-image: url(./storage/TECH.png);">
+                <div class="divImg"   >
+               <div class="boxDivImgGlass">
+                    <h2>  TECH</h2>
+                    <p>Scopri le ultime innovazioni e tendenze nel mondo della tecnologia. Dalle recensioni di nuovi gadget alle novità nel settore dell’IT, resta aggiornato su tutto ciò che riguarda la tecnologia e il suo impatto sul mondo.</p>
+               </div>
                 </div>
             </div>
         </div>

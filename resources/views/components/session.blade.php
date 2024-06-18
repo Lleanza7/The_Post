83cbcfecf9a86f181ed02f00e5c649d216d8d4ea
Lleanza7 <lucaleanza92@hotmail.com>
2024-06-18 20:10:21 +0200

    fix time closed popup

--- resources/views/components/session.blade.php
+++ resources/views/components/session.blade.php
@@ -1,12 +1,12 @@
 @if (session('message'))
-    <div style="margin: 0px" id="popSuccess" class="pop" x-data="{ isOpen: true }" x-init="setTimeout(() => { isOpen = false; }, 93000)" >
+    <div style="margin: 0px" id="popSuccess" class="pop" x-data="{ isOpen: true }" x-init="setTimeout(() => { isOpen = false; }, 3000)" >
         <div class="successPopup"  role="alert" x-show="isOpen" class="popup">
             {{ session('message') }}
          
         </div>
     </div>
 @elseif (session('denied'))
-    <div style="margin: 0px" id="popDenied" class="pop" x-data="{ isOpen: true }" x-init="setTimeout(() => { isOpen = false; }, 93000)" >
+    <div style="margin: 0px" id="popDenied" class="pop" x-data="{ isOpen: true }" x-init="setTimeout(() => { isOpen = false; }, 3000)" >
         <div class="deniedPopup"  role="alert" x-show="isOpen" class="popup">
             {{ session('denied') }} 
           
